87252d4af0385e511a19479861cc530525f28ff9
unknown <DALOSA01@CASER-5041.caser.local>
2017-01-04 12:07:52 +0100

    added ansible playbook

--- services/shell/shellService.js
+++ services/shell/shellService.js
@@ -1,7 +1,8 @@
 var spawn = require('child_process').spawn;
 var StringDecoder = require('string_decoder').StringDecoder;
 function runCommand(job,doneCallback){
-  var child = spawn('node', ['-v'],{shell:true });
+cwd 
+  var child = spawn('ansible-playbook', ['-i','windows',job.data.rule, '--tags','"'+job.data.tags+'"' ],{shell:true,cwd: "/datos/ansible/ansible-data/window_playbooks" });
   var decoder = new StringDecoder('utf8');
   child.stdout.on('data', function(chunk) {
     // output will be here in chunks
@@ -9,7 +10,7 @@ function runCommand(job,doneCallback){
 	job.log (textChunk);
   });
   child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log('child process exited with code ${code}');
     doneCallback();
   });
   // or if you want to send output elsewhere
