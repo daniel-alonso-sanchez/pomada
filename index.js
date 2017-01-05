d43503e267d521d288912a30e53b71f5985b7848
unknown <DALOSA01@CASER-5041.caser.local>
2017-01-05 15:32:49 +0100

    Renombrado depomada  a ceda. Añadido figlet para temas ascii

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var service = require('./services/queue/bullService');
-console.log("PO.MA.D.A is running");
+var figlet = require('figlet');
+var colors = require('colors');
+var appPackage = require('./package.json');
+ 
+figlet(appPackage.name.toUpperCase(), function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.green);
+	console.log("-Configuración de equipos de desarrollo automatizado-".yellow);
+});
--- services/shell/shellService.js
+++ services/shell/shellService.js
@@ -1,9 +1,15 @@
 var logger = require.main.require('./utils/logs/logger');
 var spawn = require('child_process').spawn;
+var config= require ('config');
 var StringDecoder = require('string_decoder').StringDecoder;
+var has = require('lodash.has');
 function runCommand(job,doneCallback){
-  var child = spawn('ansible-playbook', ['-i',job.data.to,job.data.rule, '--tags',job.data.tags ],{cwd: "/datos/ansible/ansible-data/window_playbooks" }, {stdio: "inherit"});
+  logger.debug("getting parameters... ");
+  var parameters= getParameters (job); 
+  logger.debug("before running shell command... ");
+  var child = spawn('ansible-playbook', parameters,{cwd: config.get("app").playbook.path }, {stdio: "inherit"});
   var decoder = new StringDecoder('utf8');
+  
   child.stdout.on('data', function(chunk) {
     // output will be here in chunks
     var textChunk = decoder.write(chunk);
@@ -13,10 +19,9 @@ function runCommand(job,doneCallback){
           var textChunk = decoder.write(chunk);		 
           job.log (textChunk);
   });
-  child.on('close', function (code) {
-        console.log('child process exited with code ' + code);
+  child.on('close', function (code) {        
         if (code!==0){
-				logger.debug('Job %d exited with code %d. Details: %s', job.id, code, JSON.stringify(job));
+				logger.error('Job %d exited with code %d. Details: %s', job.id, code, JSON.stringify(job));
                 throw new Error( 'job ends with code: '+ code );
         }
         doneCallback();
@@ -25,6 +30,39 @@ function runCommand(job,doneCallback){
   // or if you want to send output elsewhere
   //child.stdout.pipe(dest);
 };
+function checkTargets(machineList){
+	logger.debug("checkTargets... ");
+	var reIP = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})$|^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]))$/,
+    tokens = machineList.split(/\s*,\s*/), i;
+	
+	for (i = 0; i < tokens.length; i++){
+		if (!reIP.test(tokens[i])){
+			 logger.error("Ip address not valid: " +tokens[i]);
+			 throw new Error( 'Ip address not valid : '+ tokens[i] );
+		}
+	}
+	logger.debug("end checkTargets... ");
+	return machineList;
+}
+function getRule(requestedRule){
+	var rule="site.yml";
+	logger.debug("getRule... "+requestedRule);
+	if (requestedRule && requestedRule.length>0){		
+		rule=requestedRule;
+	}
+	logger.debug("end getRule... ");
+	return rule;
+}
+function getParameters(job){
+	var parameters=['-i',checkTargets(job.data.to),getRule(job.data.rule)];
+	var hasProperty=has(job.data, 'tags');
+	logger.debug('hasProperty:'+hasProperty);
+	if (has(job.data, 'tags') && job.data.tags.length>0){		
+		parameters=parameters.concat (['--tags',job.data.tags]);	
+	}
+	logger.debug('parameters:'+parameters);
+	return parameters;
+}
 module.exports = {
     run: runCommand
 }
